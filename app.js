08b02e4d3df6fcfbced406475c725526922b0762
Ashish <grunch.me1@gmail.com>
2025-04-26 21:42:30 +0545

    PRG is implimented

--- app.js
+++ app.js
@@ -19,22 +19,46 @@ app.get('/', (req, res) => {
 
 //Post form
 app.post('/submit', (req, res) => {
-  const { first, last, email, age, phone } = req.body;
+  const { first, last, email, phone } = req.body;
+  const age = Number(req.body.age);
 
   //Server-side validation {
   if (!first || !last || !email || !age || !phone) {
     return res.render('form', { error: 'All fields are required to process' });
   }
 
-  if (Number.isNaN(age) || age < 1 || age > 120) {
+  if (isNaN(age) || age < 1 || age > 120) {
     return res.render('form', {
       error: 'Age must be a number between 1 - 120',
     });
   }
 
-  //Stoer in temporary array
-  submissions.push({ first, last, email, age, phone });
+  //checking of data is existed
+  let submission = submissions.find((s) => s.email || s.phone);
 
+  //Store in temporary array
+  if (!submission) {
+    submission = { first, last, email, age, phone };
+    submissions.push(submission);
+  }
+
+  // Redirecting to result
+  res.redirect(`/result?email=${encodeURIComponent(email)}`);
+});
+
+//GET result
+app.get('/result', (req, res) => {
+  const { email } = req.query;
+
+  const submission = submissions.find((s) => s.email);
+
+  if (!submission) {
+    return res.redirect('/');
+  }
+
+  const { first, last, age, phone } = submission;
+
+  //Render result
   res.render('result', { first, last, email, age, phone, submissions });
 });
 
